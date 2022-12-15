45b6132e3432e9ba68412bac7a630c399353f6b6
TapiwaJoel <tapiwajoel@gmail.com>
2022-12-15 05:57:24 +0200

    project init

---
+++ src/main/java/zw/co/tapiwajoel/zombies/ZombiesApplication.java
@@ -0,0 +1,12 @@
+package zw.co.tapiwajoel.zombies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZombiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZombiesApplication.class, args);
+    }
+}
---
+++ src/test/java/zw/co/tapiwajoel/zombies/ZombiesApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.tapiwajoel.zombies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZombiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
